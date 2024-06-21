8b1e7e47eaa90db93c270a425d54e43e73ffb81d
Arka Ghosh <58136331+arkog96@users.noreply.github.com>
2024-06-21 17:11:28 -0400

    Time: 2055 ms (5.01%), Space: 0B (100.00%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+#SELECT id, movie, description, rating
+#FROM Cinema
+#WHERE MOD(id, 2) = 1
+#AND description <> 'boring'
+#ORDER BY id DESC;
+
+SELECT id, movie, description, rating
+FROM Cinema
+WHERE id % 2 = 1
+AND description != 'boring'
+ORDER BY rating DESC;
