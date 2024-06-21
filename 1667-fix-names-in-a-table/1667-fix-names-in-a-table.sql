d2cebfe0824e0bb30bafb04546cb4d83f8890318
Arka Ghosh <58136331+arkog96@users.noreply.github.com>
2024-06-21 17:09:17 -0400

    Time: 645 ms (81.64%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -2,4 +2,8 @@
 
 SELECT user_id, CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) AS name
 FROM Users
-ORDER BY user_id;
+ORDER by user_id;
+
+#SELECT user_id, CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) AS name
+#FROM Users
+#ORDER BY user_id;
